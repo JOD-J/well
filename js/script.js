8357a9d06e8028c1218efae9da9baf1a2bb18fb2
jodol <tj1998g@gmail.com>
2022-03-25 21:30:05 +0500

    lesson02

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-let money, 
-income, 
-addExpenses, 
-deposit, 
-mission, 
-period;
+let money = 50000, 
+income = 'ютуб', 
+addExpenses = 'Интернет, Такси, Коммуналка', 
+deposit = false, 
+mission = 350000, 
+period = 5;
 
-alert('hi alert');
-console.log('hi console');
+console.log('typeof: ', typeof(money));
+console.log('typeof: ', typeof(income));
+console.log('typeof: ', typeof(deposit));
+console.log('length: ', addExpenses.length);
+console.log('text: ', 'Период равен' + ' ' + period + ' ' + 'месяцев и "Цель заработать' + ' ' + mission + ' ' + 'рублей"');
+console.log('addExpenses: ', addExpenses.toLowerCase().split());
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
