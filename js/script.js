96c57552bb4c7a13c277ceecae6b42465e05e4b7
jodol <tj1998g@gmail.com>
2022-03-25 22:25:46 +0500

    lesson03

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-let money = 50000, 
-income = 'ютуб', 
-addExpenses = 'Интернет, Такси, Коммуналка', 
-deposit = false, 
-mission = 350000, 
-period = 5;
-
-console.log('typeof: ', typeof(money));
-console.log('typeof: ', typeof(income));
-console.log('typeof: ', typeof(deposit));
-console.log('length: ', addExpenses.length);
-console.log('text: ', 'Период равен' + ' ' + period + ' ' + 'месяцев и "Цель заработать' + ' ' + mission + ' ' + 'рублей"');
-console.log('addExpenses: ', addExpenses.toLowerCase().split());
-
-let budgetDay = money / 30;
+'use strict';
+let money = +prompt('Ваш месячный доход?');
+console.log('money: ', money);
+let income = 'ютуб';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log('addExpenses: ', addExpenses);
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log('deposit: ', deposit);
+let mission = 350000;
+let period = 5;
+let expenses1 = prompt('Введите обязательную статью расходов?');
+console.log('expenses1: ', expenses1);
+let amount1 = +prompt('Во сколько это обойдется?');
+console.log('amount1: ', amount1);
+let expenses2 = prompt('Введите обязательную статью расходов?');
+console.log('expenses2: ', expenses2);
+let amount2 = +prompt('Во сколько это обойдется?');
+console.log('amount2: ', amount2);
+let budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth: ', budgetMonth);
+let monthsMission = (mission / budgetMonth);
+console.log('monthsMission: ', monthsMission);
+console.log('ceilMonthsMission: ', Math.ceil(monthsMission));
+let budgetDay = budgetMonth / 30;
 console.log('budgetDay: ', budgetDay);
+console.log('floorBudgetDay: ', Math.floor(budgetDay));
+
+if (budgetDay > 1200) {
+	console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay < 1200) {
+	console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay > 0) {
+	console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+	console.log('Что то пошло не так');
+}
