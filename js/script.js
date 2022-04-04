b7ff07fd81e8c7a0f754534f2dbe379c315010dd
jodol <tj1998g@gmail.com>
2022-04-04 21:43:48 +0500

    lesson09

--- js/script.js
+++ js/script.js
@@ -156,21 +156,73 @@ let start = function () {
 	}
 	while (!isNumber(money))
 };
-start();
+// start();
 // ? ===================================================== start ===========================================================
 
-appData.asking();
-appData.getExpensesMonth();
-appData.getBudget();
-appData.getTargetMonth();
-appData.getStatusIncome();
-appData.getInfoDeposit();
-appData.calcSavedMoney();
+// appData.asking();
+// appData.getExpensesMonth();
+// appData.getBudget();
+// appData.getTargetMonth();
+// appData.getStatusIncome();
+// appData.getInfoDeposit();
+// appData.calcSavedMoney();
 
 
 
-console.log('appData: ', appData);
+// console.log('appData: ', appData);
 
-for (let key in appData) {
-	console.log('Наша программа включает в себя данные', key, appData[key]);
-};
+// for (let key in appData) {
+// 	console.log('Наша программа включает в себя данные', key, appData[key]);
+// };
+
+let startElem = document.getElementById('start');
+console.log('startElem: ', startElem);
+let incomeAddElem = document.getElementsByTagName('button')[0];
+console.log('incomeAddElem: ', incomeAddElem);
+let expensesAddElem = document.getElementsByTagName('button')[1];
+console.log('expensesAddElem: ', expensesAddElem);
+let depositCheckElem = document.querySelector('#deposit-check');
+console.log('depositCheckElem: ', depositCheckElem);
+let additionalIncomeItemElems = document.querySelectorAll('.additional_income-item');
+console.log('additionalIncomeItemElems: ', additionalIncomeItemElems);
+
+let budgetMonthValueElem = document.getElementsByClassName('budget_month-value');
+console.log('budgetMonthValueElem: ', budgetMonthValueElem);
+let budgetDayValueElem = document.getElementsByClassName('budget_day-value');
+console.log('budgetDayValueElem: ', budgetDayValueElem);
+let expensesMonthValueElem = document.getElementsByClassName('expenses_month-value');
+console.log('expensesMonthValueElem: ', expensesMonthValueElem);
+let additionalIncomeValueElem = document.getElementsByClassName('additional_income-value');
+console.log('additionalIncomeValueElem: ', additionalIncomeValueElem);
+let additionalExpensesValueElem = document.getElementsByClassName('additional_expenses-value');
+console.log('additionalExpensesValueElem: ', additionalExpensesValueElem);
+let incomePeriodValueElem = document.getElementsByClassName('income_period-value');
+console.log('incomePeriodValueElem: ', incomePeriodValueElem);
+let targetMonthValueElem = document.getElementsByClassName('target_month-value');
+console.log('targetMonthValueElem: ', targetMonthValueElem);
+
+let salaryAmountElem = document.querySelector('.salary-amount');
+console.log('salaryAmountElem: ', salaryAmountElem);
+
+let incomeTitleElem = document.querySelectorAll('.income-title')[1];
+console.log('incomeTitleElem: ', incomeTitleElem);
+let incomeAmountElem = document.querySelector('.income-amount');
+console.log('incomeAmountElem: ', incomeAmountElem);
+
+let expensesTitleElem = document.querySelectorAll('.expenses-title')[1];
+console.log('expensesTitleElem: ', expensesTitleElem);
+let expensesAmountElem = document.querySelector('.expenses-amount');
+console.log('expensesAmountElem: ', expensesAmountElem);
+
+let depositAmountElem = document.querySelector('.deposit-amount');
+console.log('depositAmountElem: ', depositAmountElem);
+let depositPercentElem = document.querySelector('.deposit-percent');
+console.log('depositPercentElem: ', depositPercentElem);
+
+let additionalExpensesItemElem = document.querySelector('.additional_expenses-item');
+console.log('additionalExpensesItemElem: ', additionalExpensesItemElem);
+let targetAmountElem = document.querySelector('.target-amount');
+console.log('targetAmountElem: ', targetAmountElem);
+let periodSelectElem = document.querySelector('.period-select');
+console.log('periodSelectElem: ', periodSelectElem);
+
