0167db9d37965e8105ca6cccf2d3f79643b6c5a9
jodol <tj1998g@gmail.com>
2022-04-26 21:54:39 +0500

    lesson20

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict';
+// ! таймер
 // ? =============================================== countTimer ===============================================
 function countTimer(deadLine) {
 	let timerHoursElem = document.querySelector('#timer-hours');
 	let timerMinutesElem = document.querySelector('#timer-minutes');
 	let timerSecondsElem = document.querySelector('#timer-seconds');
-	
+
 	// * =============================================== getTimeRemaining ===============================================
 	function getTimeRemaining() {
 		let dateStop = new Date(deadLine).getTime();
@@ -21,7 +22,7 @@ function countTimer(deadLine) {
 		};
 	};
 	// * =============================================== getTimeRemaining ===============================================
-	
+
 
 	// * =============================================== updateClock ===============================================
 	function updateClock() {
@@ -49,6 +50,92 @@ function countTimer(deadLine) {
 	updateClock();
 	// * =============================================== updateClock ===============================================
 };
-countTimer('26 April 2022');
+countTimer('30 April 2022');
 // ? =============================================== countTimer ===============================================
+
+
+// ! менюшка
+// ? =============================================== toggleMenu ===============================================
+const toggleMenu = () => {
+	const btnMenuElem = document.querySelector('.menu');
+	const menuElem = document.querySelector('menu');
+	const closeBtnElem = document.querySelector('.close-btn');
+	const menuItemsElems = menuElem.querySelectorAll('ul>li>a');
+	const mainAElem = document.querySelector('main>a');
+	let isOpen = true;
+	let countInterval = 0;
+	// * =============================================== handlerMenu ===============================================
+	const handlerMenu = () => {
+		if (isOpen) {
+			isOpen = false;
+			if (window.innerWidth > 768) {
+				const startSetInterval = setInterval(() => {
+					countInterval++;
+					if (countInterval <= 100) {
+						menuElem.style.transform = `translateX(${countInterval}%)`;
+					} else {
+						clearInterval(startSetInterval);
+					};
+				}, 4);
+			} else {
+				menuElem.style.transform = `translateX(100%)`;
+			}
+		} else {
+			isOpen = true;
+			countInterval = 0;
+			menuElem.style.transform = `translateX(-100%)`;
+		};
+	};
+	// * =============================================== handlerMenu ===============================================
+	
+	
+	// ! плавный скрол меню и блока под майном
+	// * =============================================== scroll ===============================================
+	const scroll = (item) => {
+		const blockID = item.getAttribute('href');
+		document.querySelector(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+	};
+	// * =============================================== scroll ===============================================
+	
+	
+	// * =============================================== обработчики ===============================================
+	btnMenuElem.addEventListener('click', handlerMenu);
+	closeBtnElem.addEventListener('click', handlerMenu);
+	menuItemsElems.forEach(item => {
+		item.addEventListener('click', handlerMenu);
+		item.addEventListener('click', event => {
+			event.preventDefault();
+			scroll(item);
+		});
+	});
+	mainAElem.addEventListener('click', event => {
+		event.preventDefault();
+		scroll(mainAElem);
+	});
+	// * =============================================== обработчики ===============================================
+};
+toggleMenu();
+// ? =============================================== toggleMenu ===============================================
+
+
+// ! popup
+// ? =============================================== togglePopUp ===============================================
+const togglePopUp = () => {
+	const popupElem = document.querySelector('.popup');
+	const popupBtnElem = document.querySelectorAll('.popup-btn');
+	const popupCloseElem = document.querySelector('.popup-close');
+	popupBtnElem.forEach(item => {
+		item.addEventListener('click', () => {
+			popupElem.style.display = 'block';
+		});
+	});
+	popupCloseElem.addEventListener('click', () => {
+		popupElem.style.display = 'none';
+	});
+};
+togglePopUp();
+// ? =============================================== togglePopUp ===============================================
 
